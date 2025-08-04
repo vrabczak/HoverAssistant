47073623d255b35af90699d03048a4c56fe57d28
vrabczak <defilerus@gmail.com>
2025-08-04 21:16:10 +0200

    index.html fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'helicopter-hover-assistant.html',
+      filename: 'index.html',
       inject: 'body',
       minify: {
         removeComments: true,
